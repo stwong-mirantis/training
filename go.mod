1818bc78bd660d837b641a16519b147837dad849
Steven Wong <swong@SWONG-MBP14.local>
2022-08-30 16:53:26 -0700

    add tests for user package

--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.19
 require (
 	github.com/emicklei/go-restful/v3 v3.9.0
 	github.com/google/uuid v1.3.0
+	github.com/stretchr/testify v1.3.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 )
--- user/user_test.go
+++ user/user_test.go
@@ -1,1 +1,47 @@
 package user
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"strconv"
+	"testing"
+)
+
+func TestUserResource_RemoveUser(t *testing.T) {
+	ur := NewUserResource()
+	username := "julip"
+	expectedUser, err := ur.AddUser(username)
+	require.NoError(t, err)
+	onlineUsersBefore := ur.GetAllOnlineUsers()
+	assert.Equal(t, ur.DoesAuthTokenExist(expectedUser.UUID), true, "user exists should be true")
+	require.Len(t, onlineUsersBefore, 1)
+	ur.RemoveUser(expectedUser.UUID)
+	onlineUsersAfter := ur.GetAllOnlineUsers()
+	assert.Equal(t, ur.DoesAuthTokenExist(expectedUser.UUID), false, "user exists should be false")
+	require.Len(t, onlineUsersAfter, 0)
+}
+
+func TestUserResource_AddUser(t *testing.T) {
+	ur := NewUserResource()
+	onlineUsersBefore := ur.GetAllOnlineUsers()
+	require.Len(t, onlineUsersBefore, 0)
+	username := "julip"
+	user, err := ur.AddUser(username)
+	require.NoError(t, err)
+	onlineUsersAfter := ur.GetAllOnlineUsers()
+	require.Len(t, onlineUsersAfter, 1)
+	assert.Equal(t, ur.DoesAuthTokenExist(user.UUID), true, "auth token should exist")
+}
+
+func TestUserResource_GetAllOnlineUsers(t *testing.T) {
+	ur := NewUserResource()
+	var expectedUsers []User
+	for i := 0; i < 3; i++ {
+		u, err := ur.AddUser(strconv.Itoa(i))
+		require.NoError(t, err)
+		expectedUsers = append(expectedUsers, u)
+	}
+	onlineUsers := ur.GetAllOnlineUsers()
+
+	require.ElementsMatch(t, expectedUsers, onlineUsers)
+}
